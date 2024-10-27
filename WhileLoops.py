880b5a161f0c82913a010c1d466b70798502c288
im651 <im651@kent.ac.uk>
2024-10-27 23:08:25 +0000

    Update WhileLoops.py

--- WhileLoops.py
+++ WhileLoops.py
@@ -1,1 +1,16 @@
 ### Part Two -- your code goes here. 
+import random
+
+number = random.randint(1, 100)
+guess = 0
+
+while guess != number:
+    guess = int(input("Guess a number between 1 and 100: "))
+  
+    if guess < number:
+        print("Too low!")
+      
+    elif guess > number:
+        print("Too high!")
+      
+print("Congratulations! You've guessed the number.")
