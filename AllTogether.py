c761882a0779d6a23f1a99a50cd8ca09a3aab13c
im651 <im651@kent.ac.uk>
2024-10-27 23:03:15 +0000

    Update AllTogether.py

--- AllTogether.py
+++ AllTogether.py
@@ -1,1 +1,17 @@
 ### Part Four -- your code goes here. 
+import random
+
+random_numbers = [random.randint(1, 100) for _ in range(10)]
+print("Original list of random_numbers :", random_numbers)
+
+
+index = 0
+
+while index < len(random_numbers):
+    
+    if random_numbers[index] % 2 == 0:
+         random_numbers.pop(index)
+         
+    else:
+        index += 1
+        print("Remaining odd numbers:", random_numbers)
